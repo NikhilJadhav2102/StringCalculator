2810570bf268ac901f2e217e2cdc74e836e1ca02
Nikhil jadhav <nikhils2102@gmail.com>
2021-10-16 19:40:39 +0530

    first commit

---
+++ src/test/java/Calculator/StringCalculatorShould.java
@@ -0,0 +1,27 @@
+package Calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StringCalculatorShould {
+
+    @Test
+    public void empty_string_should_return_0() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void string_with_single_number_should_return_number_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(1, stringCalculator.add("1"));
+    }
+    
+    @Test
+    public void string_with_Two_number_should_return_number_as_int() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(3, stringCalculator.add("1,2"));
+    }
+        
+}
